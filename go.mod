bd7d07582fa604dbcd19b960820583be7b91cfcb
Kootkh <veprikov.s@gmail.com>
2024-01-27 00:24:32 +0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module http-rest-api
+
+go 1.21.6
---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,13 @@
+package apiserver
+
+// Описание структуры APIServer ...
+type APIServer struct {
+}
+
+// NewAPIServer  - конфигураторе, создание APIServer..
+//
+// Возвращает указатель на новый сконфигурировнный инстанс APIServer.
+func NewAPIServer() *APIServer {
+	// Инициализируем APIServer
+	return &APIServer{}
+}
