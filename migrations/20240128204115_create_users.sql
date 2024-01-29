63d0e409fcc498a2a925ad6308b7aaade491d16a
Kootkh <veprikov.s@gmail.com>
2024-01-30 00:43:00 +0300

    create correct users in db

--- migrations/20240128204115_create_users.sql
+++ migrations/20240128204115_create_users.sql
@@ -13,4 +13,5 @@ CREATE TABLE users (
 DROP TABLE users;
 -- +goose StatementEnd
 
--- goose -dir migrations postgres "user=app password=passwd dbname=restapi_dev sslmode=disable" up 20240128204115_create_users.sql
+-- goose -dir migrations postgres "host=192.168.30.10 user=app_restapi_dev password=passwd dbname=restapi_dev sslmode=disable" up 20240128204115_create_users.sql
+-- goose -dir migrations postgres "host=192.168.30.10 user=app_restapi_dev password=passwd dbname=restapi_dev sslmode=disable" down 20240128204115_create_users.sql
